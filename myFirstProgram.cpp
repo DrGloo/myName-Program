fac853b5dcd7e4306f63118644c4b4cd04eb4114
Raishan Bernard <raishanbernard@outlook.com>
2017-08-02 10:27:46 -0400

    Create myFirstProgram.cpp

---
+++ myFirstProgram.cpp
@@ -0,0 +1,10 @@
+#include "Main.h"
+using namespace std;
+
+int main() //myMainFunction
+{
+	string myName;
+	cout << "Enter your name" << endl;
+	cin >> myName;
+	cout << "Hey there " << myName << endl;
+}
